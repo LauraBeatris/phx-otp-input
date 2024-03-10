5f531e7e4852a382d02e42928e8fa52fa4a365d7
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-03-10 20:47:50 -0300

    Render draft of LiveComponent

--- lib/phx_otp_input.ex
+++ lib/phx_otp_input.ex
@@ -7,9 +7,35 @@ defmodule PhxOtpInput do
   The `PhxOtpInput` component is rendered by calling the `otp_input/1` function and passing it a form field.
   """
 
+  attr :field, :any,
+    required: true,
+    doc: "a Phoenix.HTML.FormField struct identifying the form's field"
+
+  attr :id, :string,
+    doc:
+      ~S(an id to assign to the component. If none is provided, `#{form_name}_#{field}_otp_input_component` will be used)
+
   def otp_input(assigns) do
     assigns =
-      assigns |> assign(module: PhxOtpInput.Component)
+      assigns
+      |> update(:field, fn
+        %Phoenix.HTML.FormField{} = field, _ ->
+          field
+
+        field, %{form: form} ->
+          IO.warn(
+            "instead of passing separate form and field attributes, pass a single field attribute of type Phoenix.HTML.FormField"
+          )
+
+          to_form(form)[field]
+
+        _, _ ->
+          raise "if you pass field as atom or string, you also have to pass a form"
+      end)
+      |> assign_new(:id, fn %{field: field} ->
+        "#{field.form.name}_#{field.field}_otp_input_component"
+      end)
+      |> assign(module: PhxOtpInput.Component)
 
     ~H"""
     <.live_component {assigns} />
--- lib/support/phx_otp_input_web/live/demo.ex
+++ lib/support/phx_otp_input_web/live/demo.ex
@@ -1,3 +1,13 @@
 defmodule PhxOtpInputWeb.Demo do
   use PhxOtpInputWeb, :live_view
+
+  import PhxOtpInput
+
+  @impl true
+  def mount(_params, _session, socket) do
+    socket =
+      socket |> assign(form: to_form(%{}))
+
+    {:ok, socket}
+  end
 end
