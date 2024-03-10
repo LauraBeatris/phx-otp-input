e0aa8df10754e4f257e1b2264f2505cb4f7bc8fb
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-03-10 19:07:52 -0300

    Do not compile web modules when used as dependency

--- mix.exs
+++ mix.exs
@@ -4,8 +4,10 @@ defmodule PhxOtpInput.MixProject do
   def project do
     [
       app: :phx_otp_input,
+      name: "OtpInput",
       version: "0.1.0",
       elixir: "~> 1.14",
+      description: "A Phoenix LiveView OTP input. With a layer on top of input-otp JS library.",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
@@ -25,7 +27,15 @@ defmodule PhxOtpInput.MixProject do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_), do: ["lib"]
+
+  defp elixirc_paths(:prod) do
+    # Do not compile web modules when used as dependency
+    ["lib/live_select", "lib/live_select.ex"]
+  end
+
+  defp elixirc_paths(_) do
+    ["lib"]
+  end
 
   # Specifies your project dependencies.
   #
@@ -37,16 +47,9 @@ defmodule PhxOtpInput.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.20.2"},
       {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.8.3"},
-      {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.2", runtime: Mix.env() == :dev},
-      {:heroicons,
-       github: "tailwindlabs/heroicons",
-       tag: "v2.1.1",
-       sparse: "optimized",
-       app: false,
-       compile: false,
-       depth: 1},
+      # TODO - Add demo environment
+      {:esbuild, "~> 0.8", only: [:dev, :test, :demo]},
+      {:tailwind, "~> 0.2", only: :dev, optional: true},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
@@ -64,12 +67,12 @@ defmodule PhxOtpInput.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "assets.setup", "assets.build"],
-      "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
-      "assets.build": ["tailwind phx_otp_input", "esbuild phx_otp_input"],
+      setup: ["deps.get", "cmd --cd assets yarn"],
+      "assets.package": ["esbuild package"],
       "assets.deploy": [
-        "tailwind phx_otp_input --minify",
-        "esbuild phx_otp_input --minify",
+        "cmd --cd assets yarn",
+        "tailwind default --minify",
+        "esbuild default --minify",
         "phx.digest"
       ]
     ]
